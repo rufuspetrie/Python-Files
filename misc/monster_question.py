d94701608fa1521f50e78b631fb2f3bde027f785
Rufus Petrie <rufuspetrie@gmail.com>
2021-08-01 14:25:29 -0400

    test

---
+++ misc/monster_question.py
@@ -0,0 +1,12 @@
+# 1,000 people are indexed from 1 to 1,000
+# A monster comes and eats the odd indexed people
+# The remaining people are moved over one spot
+# The monster repeats this process until one is left
+# What is the original index of the final person?
+steps = 0
+x = list(range(1,1001))
+while(len(x)>1):
+	x = x[1::2]
+	steps += 1
+print("The monster took {} rounds until he finished".format(steps))
+print("The final person had index ")
---
+++ misc/test.py
@@ -0,0 +1,7 @@
+import matplotlib
+print("Enter an integer")
+x = input()
+print(x)
+
+if __name__ == '__main__':
+	print("Python is installed okay and runs on terminal")
